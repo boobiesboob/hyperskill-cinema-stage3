6824bf318a9088fb0e2893a744ec443fff48cac2
Mo$he <e.bubnov@gmx.de>
2022-11-02 03:04:26 +0100

    start the 3d stage of the cinema project

---
+++ Cinema.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Cinema {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int row = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int col = scanner.nextInt();
+
+    }
+}
