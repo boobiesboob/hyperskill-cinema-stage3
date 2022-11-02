6c50e9e04e6626aa977dd1883d3ec6395354e915
Mo$he <e.bubnov@gmx.de>
2022-11-02 05:14:56 +0100

    break the method createCinemaHall into small ones

--- Cinema.java
+++ Cinema.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 
 public class Cinema {
-    public static void createCinemaHall(int row, int col, boolean soldSeat) {
+
+    public static void printNumberOfSeats(int col) {
         System.out.println("Cinema:");
-        for (int i = 0; i < col; i++) {
+        System.out.print(" ");
+        for (int i = 1; i <= col; i++) {
+            System.out.print(" " + i);
+        }
+    }
+    public static void printNumberOfRows(int row, int col) {
+        System.out.println();
+        for (int i = 1; i <= row; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < col; j++) {
+                System.out.print("S ");
+            }
             System.out.println();
         }
-
     }
+    public static void createCinemaHall(int row, int col, boolean soldSeat) {
+        printNumberOfSeats(col);
+        if (!soldSeat) {
+            printNumberOfRows(row, col);
+        } else {
+
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +38,6 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         int col = scanner.nextInt();
 
-
+        createCinemaHall(row, col, false);
     }
 }
