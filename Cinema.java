c359393103d67eb519e5136e5df746162f7d105a
Mo$he <e.bubnov@gmx.de>
2022-11-03 12:52:54 +0100

    added new methods for price and seat calculation. only few methods are left to implement till it works properly

--- Cinema.java
+++ Cinema.java
@@ -1,43 +1,73 @@
 import java.util.Scanner;
 
 public class Cinema {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int PREMIUM_SEAT_PRICE = 10;
+    private static int ECONOMY_SEAT_PRICE = 8;
+    private static int SEATS_MERIDIAN = 60;
+    private static int hallRows;
+    private static int hallColumns;
+    private static int boughtSeatInRow;
+    private static int boughtSeatInColumn;
+    public static void defineHallSize() {
 
-    public static void printNumberOfSeats(int col) {
+        System.out.println("Enter the number of rows:");
+        int hallRows = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int hallColumns = scanner.nextInt();
+    }
+    public static void printCinemaHall() {
         System.out.println("Cinema:");
         System.out.print(" ");
-        for (int i = 1; i <= col; i++) {
+        for (int i = 1; i <= hallColumns; i++) {
             System.out.print(" " + i);
         }
-    }
-    public static void printNumberOfRows(int row, int col) {
+
         System.out.println();
-        for (int i = 1; i <= row; i++) {
+        for (int i = 1; i <= hallRows; i++) {
             System.out.print(i + " ");
-            for (int j = 0; j < col; j++) {
+            for (int j = 0; j < hallColumns; j++) {
                 System.out.print("S ");
             }
             System.out.println();
         }
     }
-    public static void createCinemaHall(int row, int col, boolean soldSeat) {
-        printNumberOfSeats(col);
-        if (!soldSeat) {
-            printNumberOfRows(row, col);
-        } else {
+    public static void defineSeatToBuy() {
+        System.out.println("Enter a row number:");
+        boughtSeatInRow = scanner.nextInt();
 
-        }
+        System.out.println("Enter a seat number in that row:");
+        boughtSeatInColumn = scanner.nextInt();
     }
+    public static int calculateSeatPrice() {
+        int ticketPrice;
+        int boughtSeat = boughtSeatInRow * boughtSeatInColumn;
+        int frontSeats;
+        int backSeats;
 
+        if (boughtSeat <= SEATS_MERIDIAN) {
+            ticketPrice = boughtSeat * PREMIUM_SEAT_PRICE;
+        } else {
+            frontSeats = boughtSeatInRow / 2 * boughtSeatInColumn;
+            backSeats = boughtSeat - frontSeats;
+            ticketPrice = frontSeats * PREMIUM_SEAT_PRICE + backSeats * ECONOMY_SEAT_PRICE;
+        }
+        return ticketPrice;
+    }
+    public static void printTicketPrice() {
+        int price = calculateSeatPrice();
+        System.out.println("Ticket price: " + price);
+    }
+    public static void showBoughtSeatInHall () {
+
+    }
     public static void main(String[] args) {
+        defineHallSize();
+        printCinemaHall();
+        defineSeatToBuy();
+        calculateSeatPrice();
+        printTicketPrice();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the number of rows:");
-        int row = scanner.nextInt();
-
-        System.out.println("Enter the number of seats in each row:");
-        int col = scanner.nextInt();
-
-        createCinemaHall(row, col, false);
     }
 }
